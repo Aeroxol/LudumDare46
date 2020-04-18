f8a108f43fddf4a61ef54577aae91403fb462060
yellowisher <auau52@naver.com>
2020-04-18 20:44:50 +0900

    sorry...

--- Assets/0_Scripts/yApy/OnTileObject.cs
+++ Assets/0_Scripts/yApy/OnTileObject.cs
@@ -9,4 +9,15 @@ public class OnTileObject : MonoBehaviour
 	public virtual void OnExitTile(Tile tile) { }
 	public virtual void OnOtherEnter(OnTileObject other) { }
 	public virtual void OnOtherExit(OnTileObject other) { }
+
+    public static void MoveTo(OnTileObject onTileObject, Tile newTile, bool adjustPosition = true)
+    {
+        if (newTile == null)              return;
+        if (onTileObject == null)         return;
+        if (onTileObject.Tile == null)    return;
+        if (onTileObject.Tile == newTile) return;
+
+        onTileObject.Tile.RemoveOnTileObject(onTileObject);
+        newTile.AddOnTileObject(onTileObject, adjustPosition);
+    }
 }
