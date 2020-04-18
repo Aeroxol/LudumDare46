98c6019be4ace3932cf2362c04b9c9c70916f915
yellowisher <auau52@naver.com>
2020-04-18 19:33:42 +0900

    tilemap

---
+++ Assets/0_Scripts/yApy/OnTileObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTileObject : MonoBehaviour
+{
+	public Tile Tile { get; set; }
+	public virtual void OnEnterTile(Tile tile) { }
+	public virtual void OnExitTile(Tile tile) { }
+	public virtual void OnOtherEnter(OnTileObject other) { }
+	public virtual void OnOtherExit(OnTileObject other) { }
+}
---
+++ Assets/0_Scripts/yApy/OnTileObjectExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class OnTileObjectExtension
+{
+    public static void Move(this OnTileObject onTileObject, Tile newTile, bool adjustPosition)
+    {
+        if (onTileObject == null) return;
+        if (onTileObject.Tile == null) return;
+
+        onTileObject.Tile.RemoveOnTileObject(onTileObject);
+        newTile.AddOnTileObject(onTileObject, adjustPosition);
+    }
+}
